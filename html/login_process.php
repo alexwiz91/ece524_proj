9279709737373d6d8ff21ea0876b2cbba7a99fe4
root <root@ip-10-28-68-192.us-west-2.compute.internal>
2018-04-11 02:39:29 +0000

    added a couple new php pages

--- html/login_process.php
+++ html/login_process.php
@@ -1,4 +1,23 @@
-<?php 
-	$user =  $_POST['username']
-	echo $user
+<?PHP 
+	include 'server_init.php';
+	$use_query = "USE ece524_proj;";	
+	$result = $conn->query($use_query);
+	$sql_query = "SELECT username, password FROM UserInfo;";
+	$result = $conn->query($sql_query);
+	if($result->num_rows > 0)
+	{
+		while($row = $result->fetch_assoc())
+		{
+			echo "username: " . $row["username"]. " - Name: " . $row["password"] . "<br>";
+		}
+	}
+	else
+	{
+		echo "zero results"."<br>";
+	}
+
+	$user =  $_POST['username'];
+	$pw =  $_POST['password'];
+	echo $user."<br>"; 
+	echo $pw."<br>";
 ?>
---
+++ html/server_init.php
@@ -0,0 +1,14 @@
+<?PHP
+$servername = "localhost";
+$username = "root";
+$password = "team1234";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully"."<br>";
+?>

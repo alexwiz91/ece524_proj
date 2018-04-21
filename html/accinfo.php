9f2e3ad7fd436e05ac79e6b6d695e873bf340f17
Ubuntu <alexwisniewski91@gmail.com>
2018-04-21 09:08:17 +0000

    account updates work!

--- html/accinfo.php
+++ html/accinfo.php
@@ -46,26 +46,38 @@ tr:nth-child(even) {
 
 <form style="font-family:sans-serif;" action="save_info.php" method="post">
   Account No: 
-  <label><?php echo $_SESSION['USERDATA']['AccNum']; ?></label> 
+  <label name='acc'><?php echo $_SESSION['USERDATA']['AccNum']; ?></label> 
+  <input type="hidden" name='lblAcc' value="<?php echo $_SESSION['USERDATA']['AccNum']; ?>"></input>
   <br>
   First Name: 
   <input type="text" name="fname" value="<?php echo $_SESSION['USERDATA']['FirstName'];?>"></input> 
   <br>
   Last Name: 
-  <input type="text"name="lname" value="<?php echo $_SESSION['USERDATA']['LastName']; ?>"></input> 
+  <input type="text" name="lname" value="<?php echo $_SESSION['USERDATA']['LastName']; ?>"></input> 
+  <br>
+  Email:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  <input type="text" name="email"  value ="<?php echo $_SESSION['USERDATA']['email']; ?>"></input> 
   <br><br>
   Street:
-  <input type="text"name="housenumber" size ="6" value="<?php echo $_SESSION['USERDATA']['HouseNum']; ?>"></input> 
-  <input type="text"name="street" value="<?php echo $_SESSION['USERDATA']['Street']; ?>"></input> 
+  <input type="text" name="housenumber" size ="6" value="<?php echo $_SESSION['USERDATA']['HouseNumber']; ?>"></input> 
+  <input type="text" name="street" value="<?php echo $_SESSION['USERDATA']['Street']; ?>"></input> 
   <br><br>
   City:    
-  <input type="text"name="city" value ="<?php echo $_SESSION['USERDATA']['City']; ?>"</input> 
+  <input type="text" name="city" value ="<?php echo $_SESSION['USERDATA']['City']; ?>"</input> 
   State: 
-  <input type="text"name="state" size = "3" value ="<?php echo $_SESSION['USERDATA']['State']; ?>"</input> 
-  <br><br>
+  <input type="text" name="state" size = "3" value ="<?php echo $_SESSION['USERDATA']['State']; ?>"</input> 
+  <br>
   Example Input Box:<br>
   <input type="text" name="rout">
   <br><br>  
+<?php
+	if($_SESSION['updatedinfo'])
+	{
+		echo '<span style="color: green;"/> Account Information Updated!</span>';
+		$_SESSION['updatedinfo'] = FALSE;
+	}
+?>
+	
  </fieldset> 
 <input name="action" type="submit"  width="80px" height="30px" font-size="8px" value="Save" formaction="save_info.php"> </button>
 <input name="action"  type="submit" width="80px" height="30px" font-size="8px" value="Cancel" formaction="save_info.php"> </button>
--- html/save_info.php
+++ html/save_info.php
@@ -2,10 +2,28 @@
 session_start();
 if($_POST['action'] == "Save")
 {
+	include "server_init.php";
 	echo "Saving Account information...";
-
-	sleep(5);
-
+	$house = $_POST['housenumber'];
+	$street = $_POST['street'];	
+	$fname = $_POST['fname'];	
+	$lname = $_POST['lname'];	
+	$city = $_POST['city'];	
+	$state = $_POST['state'];	
+	$email = $_POST['email'];	
+	$acc = $_POST['lblAcc'];
+	$sql_query="use ece524_proj";
+	$conn->query($sql_query);
+	$sql_query= "UPDATE UserInfo SET firstname='" . $fname . "'," . "lastname='" . $lname . "'," . "city='" . $city . "', state='" . $state . "', email='" . $email . "',housenumber='" . $house . "',street='" . $street . "' where AccNum='" . $acc . "'";
+	$result = $conn->query($sql_query);
+	if($result)
+	{
+		$_SESSION['updatedinfo'] = TRUE;
+	}
+	$sql_query="SELECT * FROM UserInfo where AccNum='" . $acc . "';";
+	$result = $conn->query($sql_query);
+	$_SESSION['USERDATA'] = $result->fetch_assoc();
+	echo "Headed back to account information page";
 	header("Location: accinfo.php");
 	exit();
 }

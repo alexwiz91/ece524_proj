ae587ab42127b0a5f8fd6de9ed283b91db2250b1
Ubuntu <alexwisniewski91@gmail.com>
2018-04-23 04:43:25 +0000

    fixed cancel

--- html/create_account.php
+++ html/create_account.php
@@ -1,46 +1,54 @@
 
 <?PHP
-	include 'server_init.php';
-	include 'common_db.php';
-	$firstname = $_POST['firstname'];
-	$lastname = $_POST['lastname'];
-	$username = $_POST['username'];
-	$pw = $_POST['password'];
-	$use_query = "USE ece524_proj;";	
-	$result = $conn->query($use_query);
-	$insert_attempt = "SELECT * FROM UserInfo where username='". $username . "'";
-	$result = $conn->query($insert_attempt);
-	if($result->num_rows > 0)
+	if($_POST['action'] = "Cancel")
 	{
-		echo $username . " already exists in the database. Please go back and try another username.";
-		echo "<br>";
+		header("Location: index.html");
+		exit();
 	}
 	else
 	{
-		$sql_query = "INSERT INTO UserInfo (FirstName, LastName, username, password) VALUES (" . "\"".$firstname."\"" . ", " . "\"". $lastname  ."\"". ", " . "\"". $username ."\"" . ", " . "\"". $pw ."\"" . ");";
-		if($conn->query($sql_query) == TRUE)
+		include 'server_init.php';
+		include 'common_db.php';
+		$firstname = $_POST['firstname'];
+		$lastname = $_POST['lastname'];
+		$username = $_POST['username'];
+		$pw = $_POST['password'];
+		$use_query = "USE ece524_proj;";	
+		$result = $conn->query($use_query);
+		$insert_attempt = "SELECT * FROM UserInfo where username='". $username . "'";
+		$result = $conn->query($insert_attempt);
+		if($result->num_rows > 0)
 		{
-			echo "Username " . $username . "created in database";
+			echo $username . " already exists in the database. Please go back and try another username.";
 			echo "<br>";
 		}
 		else
 		{
-			 echo "Error: " . $sql_query . "<br>" . $conn->error;
-		}
-		$sql_query = "SELECT * FROM UserInfo;";
-		$select_result = $conn->query($sql_query);
-		if($select_result->num_rows > 0)
-		{
-			while($row = $select_result->fetch_assoc())
+			$sql_query = "INSERT INTO UserInfo (FirstName, LastName, username, password) VALUES (" . "\"".$firstname."\"" . ", " . "\"". $lastname  ."\"". ", " . "\"". $username ."\"" . ", " . "\"". $pw ."\"" . ");";
+			if($conn->query($sql_query) == TRUE)
 			{
-				echo "username: " . $row["username"]. " - password: " . $row["password"] ."First Name: " . $row["firstname"]."<br>";
+				echo "Username " . $username . "created in database";
+				echo "<br>";
 			}
+			else
+			{
+				 echo "Error: " . $sql_query . "<br>" . $conn->error;
+			}
+			$sql_query = "SELECT * FROM UserInfo;";
+			$select_result = $conn->query($sql_query);
+			if($select_result->num_rows > 0)
+			{
+				while($row = $select_result->fetch_assoc())
+				{
+					echo "username: " . $row["username"]. " - password: " . $row["password"] ."First Name: " . $row["firstname"]."<br>";
+				}
+			}
+			else
+			{
+				echo "Reporting out table failed"."<br>";
+			}
+			$select_result->close();
 		}
-		else
-		{
-			echo "Reporting out table failed"."<br>";
-		}
-		$select_result->close();
 	}
 
 ?>

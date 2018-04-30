b7f9a67460b491467fd14069155575f9e74cef68
root <root@ip-10-28-68-192.us-west-2.compute.internal>
2018-04-30 05:07:16 +0000

    debug information added to login

--- html/login_process.php
+++ html/login_process.php
@@ -20,7 +20,15 @@
 	}
 	else
 	{
-		echo "INVALID USERNAME/PASSWORD!";
+		echo "Username / Password does not exist<br>";
+		echo "<br><br><br><br><br><br><br><br>";
+		echo "<b>Debug Information:</b><br>";
+		echo "<b>Database name:</b> ece524_proj<br>";
+		echo "<b>Table name:</b> UserInfo<br>";
+		$curr_ip = exec("hostname -i");
+		echo "<b>IP Address:</b>" . $curr_ip . "<br>";
+		echo "<b>Problematic Query:</b><br>";
+		echo $sql_query;
 	}
 //	if($result->num_rows > 0)
 //	{
